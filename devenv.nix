835fe5bbfa6313644e9395b3700f45b9e9d0e43e
octoclonius <25781800+octoclonius@users.noreply.github.com>
2026-05-31 20:11:19 -0500

    Initial commit

---
+++ devenv.nix
@@ -0,0 +1,38 @@
+_: {
+  # https://devenv.sh/git-hooks/
+  git-hooks = {
+    hooks = {
+      actionlint = {
+        enable = true;
+      };
+      clippy = {
+        enable = true;
+      };
+      check-yaml = {
+        enable = true;
+      };
+      deadnix = {
+        enable = true;
+      };
+      nixfmt = {
+        enable = true;
+      };
+      shellcheck = {
+        enable = true;
+      };
+      statix = {
+        enable = true;
+      };
+      rustfmt = {
+        enable = true;
+      };
+    };
+  };
+
+  # https://devenv.sh/languages/
+  languages = {
+    rust.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
